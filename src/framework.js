35447a1ff5f5dc3c81bcfac8b406a8a5156aa54c
vtsukur <flushdia@gmail.com>
2023-05-09 14:04:36 +0300

    [refactoring] Renaming unused variables (https://docs.github.com/en/copilot/getting-started-with-github-copilot)

--- src/framework.js
+++ src/framework.js
@@ -6,6 +6,6 @@ app.get('/', (_req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 // Return the current time
-app.get('/time', (req, res) => {
+app.get('/time', (_req, res) => {
   res.send(new Date());
 });
